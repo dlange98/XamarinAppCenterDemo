edcbe0103a57a35b98734138818974d43214f129
Dan Lange <dlange@Dans-Machine.zoomtown.com>
2017-12-16 08:16:11 -0500

    Resolve issue the authentication call not returning in android.

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -31,6 +31,8 @@ namespace Refapp.Droid
 			global::Microsoft.IdentityModel.Clients.ActiveDirectory.Resource.String.ApplicationName = global::Refapp.Droid.Resource.String.ApplicationName;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::Refapp.Droid.Resource.String.ApplicationName;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::Refapp.Droid.Resource.String.Hello;
+			global::SQLite.Net.Platform.XamarinAndroidN.Resource.String.ApplicationName = global::Refapp.Droid.Resource.String.ApplicationName;
+			global::SQLite.Net.Platform.XamarinAndroidN.Resource.String.Hello = global::Refapp.Droid.Resource.String.Hello;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Refapp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
