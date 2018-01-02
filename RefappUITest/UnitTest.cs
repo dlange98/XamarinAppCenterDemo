1c7b3205ee5141e6055926c66e603964a8b1cf9a
Audibert, Joshua <jaudibert@cardinalsolutions.com>
2018-01-02 15:38:56 -0500

    Update unit Test

--- RefappUITest/UnitTest.cs
+++ RefappUITest/UnitTest.cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
 using System;
 using Refapp.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Refapp.Services;
+using Xamarin.UITest;
 
 namespace RefappUITest
 {
@@ -14,6 +18,30 @@ namespace RefappUITest
 
             x.Title = "dan was here";
             Assert.AreEqual("dan was here", x.Title);
+        }
+
+        [Test()]
+        public async Task SettingOrderPropertyShouldRaisePropertyChanged()
+        {
+            bool invoked = false;
+            var baseViewModel = new AboutViewModel();
+
+            baseViewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName.Equals("IsBusy"))
+                    invoked = true;
+                Assert.True(true);
+            };
+
+            baseViewModel.IsBusy = true;
+
+            await Task.Delay(500);
+
+            if (invoked == false)
+            {
+                Assert.True(false);
+            }
+
         }
     }
 }
