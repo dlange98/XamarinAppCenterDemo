5af4286adc66946695df6aceb06533a9badcda9f
Dan Lange <dlange@Dans-Machine.local>
2018-01-05 13:08:27 -0500

    update android post build file with new env variable.

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -1,11 +1,11 @@
 echo "-= Build nUnit test projects: =-"
-find . -regex '.*\RefApp\.NUnitTest\.csproj' -exec msbuild {} \;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*\RefApp\.NUnitTest\.csproj' -exec msbuild {} \;
 echo
 echo "-= Found projects to run nUnit tests: =-"
-find . -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec echo {} \;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec echo {} \;
 echo
 echo "-= Running nUnit tests: =-"
-find . -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec nunit-console {} \;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec nunit-console {} \;
 echo
 echo "-= nUnit test result: =-"
-find . -name 'TestResult.xml' -exec cat {} \;
+find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml' -exec cat {} \;
