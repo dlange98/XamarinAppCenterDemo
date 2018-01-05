472f3b83c7a76aeef197bceedc7c15b3029bc8aa
Dan Lange <dlange@Dans-Machine.local>
2018-01-05 12:59:15 -0500

    update post build script

--- iOS/appcenter-post-build.sh
+++ iOS/appcenter-post-build.sh
@@ -1,11 +1,11 @@
 echo "-= Build nUnit test projects: =-"
-find . -regex '.*\RefApp\.NUnitTest\.csproj' -exec msbuild {} \;
+find ../ -regex '.*\RefApp\.NUnitTest\.csproj' -exec msbuild {} \;
 echo
 echo "-= Found projects to run nUnit tests: =-"
-find . -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec echo {} \;
+find ../ -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec echo {} \;
 echo
 echo "-= Running nUnit tests: =-"
-find . -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec nunit-console {} \;
+find ../ -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec nunit-console {} \;
 echo
 echo "-= nUnit test result: =-"
-find . -name 'TestResult.xml' -exec cat {} \;
+find ../ -name 'TestResult.xml' -exec cat {} \;
