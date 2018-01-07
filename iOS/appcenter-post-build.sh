af09959147dab0e852a400dc1be2711efb4146e8
Dan Lange <dlange@Dans-Machine.local>
2018-01-07 10:51:34 -0500

    update unit test scripts.

--- iOS/appcenter-post-build.sh
+++ iOS/appcenter-post-build.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
 echo "-= Build nUnit test projects: =-"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*\RefApp\.NUnitTest\.csproj' -exec msbuild {} \;
 echo
+
 echo "-= Found projects to run nUnit tests: =-"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec echo {} \;
 echo
+
 echo "-= Running nUnit tests: =-"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*\RefApp\.NUnitTest\.dll' -exec nunit-console {} \;
 echo
+
 echo "-= nUnit test result: =-"
-find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml' -exec cat {} \;
+pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
+cat $pathOfTestResults
+
+#look for a failing test
+grep -q 'success="False"' $pathOfTestResults
+
+if [[ $? -eq 0 ]]
+    then echo "a test Failed" exit 1
+    else echo "all tests passed" exit 0
+fi
