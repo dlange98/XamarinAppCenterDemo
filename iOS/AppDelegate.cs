c1e47dc912fa6bb1079c33de33604cd06e2b23f6
Dan Lange <dlange@Dans-Machine.local>
2018-01-10 15:58:34 -0500

    add code for push notifications for iOS.

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -3,6 +3,11 @@ using UIKit;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using WindowsAzure.Messaging;
+using System;
+using Refapp.iOS.Configuration;
+using Xamarin.Forms;
+
 
 // Comment
 namespace Refapp.Services.iOS
@@ -10,6 +15,8 @@ namespace Refapp.Services.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private SBNotificationHub Hub { get; set; }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
@@ -21,7 +28,86 @@ namespace Refapp.Services.iOS
             AppCenter.Start("29397e44-73e2-4fd1-a23f-0a32e118063f",
                    typeof(Analytics), typeof(Crashes));
 
+            configurePushNotifications();
+           
+
             return base.FinishedLaunching(app, options);
+        }
+
+        private void configurePushNotifications() 
+        {
+            if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                       UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                       new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Hub = new SBNotificationHub(Constants.ListenConnectionString, Constants.NotificationHubName);
+
+            Hub.UnregisterAllAsync(deviceToken, (error) => {
+                if (error != null)
+                {
+                    Console.WriteLine("Error calling Unregister: {0}", error.ToString());
+                    return;
+                }
+
+                NSSet tags = null; // create tags if you want
+                Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) => {
+                    if (errorCallback != null)
+                        Console.WriteLine("RegisterNativeAsync error: " + errorCallback.ToString());
+                });
+            });
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+            if (null != options && options.ContainsKey(new NSString("aps")))
+            {
+                //Get the aps dictionary
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+                string alert = string.Empty;
+
+                //Extract the alert text
+                // NOTE: If you're using the simple alert by just specifying
+                // "  aps:{alert:"alert msg here"}  ", this will work fine.
+                // But if you're using a complex alert with Localization keys, etc.,
+                // your "alert" object from the aps dictionary will be another NSDictionary.
+                // Basically the JSON gets dumped right into a NSDictionary,
+                // so keep that in mind.
+                if (aps.ContainsKey(new NSString("alert")))
+                    alert = (aps[new NSString("alert")] as NSString).ToString();
+
+                //If this came from the ReceivedRemoteNotification while the app was running,
+                // we of course need to manually process things like the sound, badge, and alert.
+                if (!fromFinishedLaunching)
+                {
+                    //Manually show an alert
+                    if (!string.IsNullOrEmpty(alert))
+                    {
+                        Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Notification", alert, "OK");
+                    }
+                }
+            }
         }
     }
 }
