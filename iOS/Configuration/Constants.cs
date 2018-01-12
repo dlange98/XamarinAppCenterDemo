d7c89638815edeafe19c984e95f561805f8a8698
Dan Lange <dlange@Dans-Machine.local>
2018-01-12 15:15:25 -0500

    Add constants for notification hub

---
+++ iOS/Configuration/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Refapp.iOS.Configuration
+{
+    public class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://referenceappnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=68+L8EhRO1QenJlkoFi5DErckR5ylD1qrpL5A5OMQYY=";
+        public const string NotificationHubName = "ReferenceAppHub";
+
+    }
+}
